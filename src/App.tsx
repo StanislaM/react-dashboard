93cef1369c014840581862af8a86d4284fdf6dc6
Mokhoid Stanislav <mohoidstanislav@gmail.com>
2023-10-13 15:05:44 +0300

    added base files and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,63 @@
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+
 import './App.scss';
+import Users from './pages/Users/Users';
+import Products from './pages/Products/Products';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import Menu from './components/Menu/Menu';
+import Login from './pages/Login/Login';
 
 const App = () => {
-    return <div className="app"></div>;
+    const Layout = () => {
+        return (
+            <div className="main">
+                <Navbar />
+                <div className="container">
+                    <div className="menuContainer">
+                        <Menu />
+                    </div>
+                    <div className="contentContainer">
+                        <Outlet />
+                    </div>
+                </div>
+                <Footer />
+            </div>
+        );
+    };
+
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <Layout />,
+            children: [
+                {
+                    path: '/',
+                    element: <Home />,
+                },
+                {
+                    path: 'users',
+                    element: <Users />,
+                },
+                {
+                    path: 'products',
+                    element: <Products />,
+                },
+            ],
+        },
+        {
+            path: 'login',
+            element: <Login />,
+        },
+    ]);
+
+    return (
+        <div className="app">
+            <RouterProvider router={router} />
+        </div>
+    );
 };
 
 export default App;
