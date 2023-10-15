920bd6816442dbe2c2609cc3a548f68bda773b2f
Mokhoid Stanislav <mohoidstanislav@gmail.com>
2023-10-15 17:14:30 +0300

    added bar charts

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,9 @@
+import BarChartBox from '../../components/BarChartBox/BarChartBox';
 import ChartBox from '../../components/ChartBox/ChartBox';
 import TopBox from '../../components/TopBox/TopBox';
 import {
+    barChartBoxRevenue,
+    barChartBoxVisit,
     chartBoxConversion,
     chartBoxProduct,
     chartBoxRevenue,
@@ -20,7 +23,7 @@ const Home = () => {
             <div className="box box3">
                 <ChartBox {...chartBoxProduct} />
             </div>
-            <div className="box box4"></div>
+            <div className="box box4">Box4</div>
             <div className="box box5">
                 <ChartBox {...chartBoxConversion} />
             </div>
@@ -28,8 +31,12 @@ const Home = () => {
                 <ChartBox {...chartBoxRevenue} />
             </div>
             <div className="box box7">Box7</div>
-            <div className="box box8">Box8</div>
-            <div className="box box9">Box9</div>
+            <div className="box box8">
+                <BarChartBox {...barChartBoxVisit} />
+            </div>
+            <div className="box box9">
+                <BarChartBox {...barChartBoxRevenue} />
+            </div>
         </div>
     );
 };
