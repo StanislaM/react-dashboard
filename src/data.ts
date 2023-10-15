a16914b674718c1d88d965b2de894e9aa269b329
Mokhoid Stanislav <mohoidstanislav@gmail.com>
2023-10-15 17:39:05 +0300

    added pie chart

--- src/data.ts
+++ src/data.ts
@@ -309,6 +309,17 @@ export const barChartBoxVisit = {
     ],
 };
 
+export const pieChartBoxSource = {
+    title: 'Leads by Source',
+    dataKey: 'value',
+    chartData: [
+        { name: 'Mobile', value: 400, color: '#0088FE' },
+        { name: 'Desktop', value: 300, color: '#00C49F' },
+        { name: 'Laptop', value: 300, color: '#FFBB28' },
+        { name: 'Tablet', value: 200, color: '#FF8042' },
+    ],
+};
+
 export const userRows = [
     {
         id: 1,
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import BarChartBox from '../../components/BarChartBox/BarChartBox';
 import ChartBox from '../../components/ChartBox/ChartBox';
+import PieChartBox from '../../components/PieChartBox/PieChartBox';
 import TopBox from '../../components/TopBox/TopBox';
 import {
     barChartBoxRevenue,
@@ -8,6 +9,7 @@ import {
     chartBoxProduct,
     chartBoxRevenue,
     chartBoxUser,
+    pieChartBoxSource,
 } from '../../data';
 import './Home.scss';
 
@@ -23,7 +25,9 @@ const Home = () => {
             <div className="box box3">
                 <ChartBox {...chartBoxProduct} />
             </div>
-            <div className="box box4">Box4</div>
+            <div className="box box4">
+                <PieChartBox {...pieChartBoxSource} />
+            </div>
             <div className="box box5">
                 <ChartBox {...chartBoxConversion} />
             </div>
